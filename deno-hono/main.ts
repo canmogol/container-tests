a3b2a0d53c4d7fc82bd7297006b1c0904533a6fe
Can MOGOL <canmogol@gmail.com>
2024-10-01 18:27:01 +0300

    Update main.ts

--- deno-hono/main.ts
+++ deno-hono/main.ts
@@ -1,9 +1,14 @@
-import { Context, Hono } from 'hono'
+import {Context, Hono} from 'hono'
 
 const app = new Hono()
 
-app.get('/', (c:Context) => {
-  return c.text('Hello World')
+app.get('/', (c: Context) => {
+    return c.text('Hello World')
+}).get('/external', async (c: Context) => {
+    const externalUrl = Deno.env.get("EXTERNAL_URL")
+    const resp = await fetch(externalUrl);
+    const data =  await resp.text()
+    return c.text(await data)
 })
 
-Deno.serve({ port: 8080 }, app.fetch) 
+Deno.serve({port: 8080}, app.fetch)
