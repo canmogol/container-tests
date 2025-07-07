facf5aabbcb6dd427a031a439d20d43269d2e3fd
Can MOGOL <canmogol@gmail.com>
2025-07-08 00:14:19 +0300

    Update main.ts

--- deno-hono/main.ts
+++ deno-hono/main.ts
@@ -5,7 +5,7 @@ const app = new Hono()
 app.get('/', (c: Context) => {
     return c.text('Hello World')
 }).get('/external', async (c: Context) => {
-    const externalUrl = Deno.env.get("EXTERNAL_URL")
+    const externalUrl = Deno.env.get("EXTERNAL_URL") || 'http://172.17.0.1:9090';
     const resp = await fetch(externalUrl);
     const data =  await resp.text()
     return c.text(await data)
