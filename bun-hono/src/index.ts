13ae0a8fa2a92488e4409cfc8d8d698a976b74ce
Can MOGOL <canmogol@gmail.com>
2025-07-08 00:11:46 +0300

    Update index.ts

--- bun-hono/src/index.ts
+++ bun-hono/src/index.ts
@@ -2,16 +2,21 @@ import { Context, Hono } from 'hono'
 import process from "node:process";
 
 const app = new Hono()
+const url = process.env.EXTERNAL_URL || 'http://172.17.0.1:9090';
 
 app.get('/', (c:Context) => {
   return c.text('Hello Hono Bun!')
 }).get('/external', async (c:Context) => {
-  const resp = await fetch(process.env.EXTERNAL_URL);
-  const data =  await resp.text()
-  return c.text(await data)
+  const resp = await fetch(url);
+  const data = await resp.text()
+  return c.text(data)
 })
 
-export default app
+
+export default {
+  port: 3000,
+  fetch: app.fetch,
+}
 
 process.on("SIGINT", () => {
   console.log("Ctrl-C was pressed");
